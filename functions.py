65c5d16f715eab12107e61f25002526ffcd39a20
unknown <sspdav02@gmail.com>
2021-07-02 15:35:08 +0530

    'adding_different_classifiers'

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
 """This module contains the functions"""
 
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
-def prediction(svc_model, SepalLength, SepalWidth, PetalLength, PetalWidth):
+def prediction(model, SepalLength, SepalWidth, PetalLength, PetalWidth):
     """This function returns the preddicted value."""
-    species = svc_model.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
+    species = model.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
     species = species[0]
     if species == 0:
         return "Iris-setosa"
@@ -13,9 +15,31 @@ def prediction(svc_model, SepalLength, SepalWidth, PetalLength, PetalWidth):
     else:
         return "Iris-versicolor"
 
-def m_score(X_train, y_train):
-    """This function creates the model the returns the model score."""
+def svc_score(X_train, y_train):
+    """This function process svc model"""
     svc_model = SVC(kernel = 'linear')
     svc_model.fit(X_train, y_train)
     score = svc_model.score(X_train, y_train)
     return svc_model, score
+
+def rf_score(X_train, y_train):
+    """This function process rf_clf model"""
+    rf_clf = RandomForestClassifier(n_jobs = -1, n_estimators = 100)
+    rf_clf.fit(X_train, y_train)
+    score = rf_clf.score(X_train, y_train)
+    return rf_clf, score
+
+def lr_score(X_train, y_train):
+    """This function process rf_clf model"""
+    log_reg = LogisticRegression(n_jobs = -1)
+    log_reg.fit(X_train, y_train)
+    score = log_reg.score(X_train, y_train)
+    return log_reg, score
+
+def clf_s(clf):
+    if clf == 'Support Vector Machine':
+        return svc_score(X_train, y_train)
+    elif clf == 'Logistic Regression':
+        return lr_score(X_train, y_train)
+    else:
+        return rf_score(X_train, y_train)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from prep_data import X_train, X_test, y_train, y_test
-from functions import prediction, m_score
+from prep_data import iris_df, X_train, X_test, y_train, y_test
+from functions import prediction, m_score, clf_selector
 
 # Creating the SVC model and storing the accuracy score in a variable 'score'.
 svc_model, score = m_score(X_train, y_train)
@@ -8,16 +8,24 @@ svc_model, score = m_score(X_train, y_train)
 # Add title widget
 st.title("Iris Flower Species Prediction App")  
 
+# Add sidbar title
+st.sidebar.title("Iris Flower Species Prediction App")
+
 # Add 4 sliders and store the value returned by them in 4 separate variables.
-s_length = st.slider("Sepal Length", 0.0, 10.0)
-s_width = st.slider("Sepal Width", 0.0, 10.0)
-p_length = st.slider("Petal Length", 0.0, 10.0)
-p_width = st.slider("Petal Width", 0.0, 10.0)
+s_len = st.sidebar.slider("Sepal Length", float(iris_df["SepalLengthCm"].min()), float(iris_df["SepalLengthCm"].max()))
+s_wid = st.sidebar.slider("Sepal Width", float(iris_df["SepalWidthCm"].min()), float(iris_df["SepalWidthCm"].max()))
+p_len = st.sidebar.slider("Petal Length", float(iris_df["PetalLengthCm"].min()), float(iris_df["PetalLengthCm"].max()))
+p_wid = st.sidebar.slider("Petal Width", float(iris_df["PetalWidthCm"].min()), float(iris_df["PetalWidthCm"].max()))
+
+# Add Classifier selector
+clf = st.sidebar.selectbox('Classifier',('Support Vector Machine', 'Logistic Regression', 'Random Forest Classifier'))
+model = clf_selector(clf)
 
 # When 'Predict' button is pushed, the 'prediction()' function must be called 
 # and the value returned by it must be stored in a variable, say 'species_type'. 
 # Print the value of 'species_type' and 'score' variable using the 'st.write()' function.
 if st.button("Predict"):
-	species_type = prediction(svc_model, s_length, s_width, p_length, p_width)
+	species_type = prediction(model, s_len, s_wid, p_len, p_wid)
 	st.write("Species predicted:", species_type)
+	st.write('Classifier used:', model)
 	st.write("Accuracy score of this model is:", score)
